0b308cc491b2c5ab7689ae277d4d16a6806daedb
Ngo Xuan Bach <thanbaiks@gmail.com>
2020-11-07 18:52:50 +0700

    chore: fix lint

--- src/env.ts
+++ src/env.ts
@@ -2,4 +2,4 @@ export const DEV = process.env.NODE_ENV === 'development';
 export const REVISION = process.env.APP_REVISION ?? 'development';
 export const MEDIA_SOURCE = 'https://minio.ngobach.com/vinyl/';
 export const LOG_ENABLED = true;
-export const LOADER_SKIPPED: boolean = DEV;
+export const LOADER_SKIPPED = DEV;
