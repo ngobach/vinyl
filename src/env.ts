11d64efcea184f050f15d28138ca0e0789134d13
Ngo Xuan Bach <thanbaiks@gmail.com>
2021-06-26 22:08:45 +0700

    fix: move back to minio

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 export const DEV = process.env.NODE_ENV === 'development';
 export const REVISION = process.env.APP_REVISION ?? 'development';
-export const MEDIA_SOURCE = 'https://files.matmang.xyz/_static/vinyl';
+export const MEDIA_SOURCE = 'https://minio.matmang.xyz/vinyl';
 export const LOG_ENABLED = true;
 export const LOADER_SKIPPED = DEV;
 export const DEFAULT_THUMBNAIL = 'https://files.matmang.xyz/_static/web-assets/chilling.webp';
