7041634cc4671e941b5803ce771f4310e607aca9
Bach Ngo <thanbaiks@gmail.com>
2023-04-02 16:48:55 +0700

    fix: change repo base

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,6 @@
 export const DEV = process.env.NODE_ENV === 'development';
 export const REVISION = process.env.APP_REVISION ?? 'development';
-export const MEDIA_SOURCE = 'https://vinyl-ngobach-com.ipns.cf-ipfs.com/';
+export const MEDIA_SOURCE = 'https://cloudflare-ipfs.com/ipns/vinyl.ngobach.com/';
 export const LOG_ENABLED = true;
 export const LOADER_SKIPPED = DEV;
 export const DEFAULT_THUMBNAIL = 'https://i.imgur.com/zqPPJBI.jpg';
