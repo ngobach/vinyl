dd0c50f9c7f6003ab54b54d8767d899ab7408537
Bach Ngo <thanbaiks@gmail.com>
2023-09-11 17:03:09 +0700

    chore: env update

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,6 @@
 export const DEV = process.env.NODE_ENV === "development";
 export const REVISION = process.env.APP_REVISION ?? "development";
-// export const MEDIA_SOURCE = 'https://cloudflare-ipfs.com/ipns/vinyl.ngobach.com/';
-export const MEDIA_SOURCE = "https://files.bachnx.cloud/Vinyl3/";
+export const MEDIA_SOURCE = "https://minio.hub.zsvr.cloud/vinyl/vinyl";
 export const LOG_ENABLED = true;
 export const LOADER_SKIPPED = DEV;
 export const DEFAULT_THUMBNAIL = "https://i.imgur.com/zqPPJBI.jpg";
